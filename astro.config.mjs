fb209c329aa8e6c0dfc79d1db01e80a65b7821ef
Pablo Dibez <pdibez@gmail.com>
2026-05-18 20:59:58 -0300

    fix: translate 404 to spanish

--- astro.config.mjs
+++ astro.config.mjs
@@ -22,6 +22,7 @@ const prettyCodeOptions = {
 // https://astro.build/config
 export default defineConfig({
   site: CONFIG.site.url,
+  base: "pdibez.github.io",
   output: 'server',
   adapter: cloudflare(),
 
