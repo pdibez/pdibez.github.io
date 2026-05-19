411aea688b20983cf11b7e12707f93e4318b0bed
Pablo Dibez <pdibez@gmail.com>
2026-05-18 21:35:20 -0300

    fix: change output build

--- astro.config.mjs
+++ astro.config.mjs
@@ -22,7 +22,7 @@ const prettyCodeOptions = {
 // https://astro.build/config
 export default defineConfig({
   site: CONFIG.site.url,
-  output: 'server',
+  output: 'static',
   adapter: cloudflare(),
 
   vite: {
