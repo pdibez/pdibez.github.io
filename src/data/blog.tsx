32f77d57191a1edebb6f9b7c4282fc4f3029670a
Pablo Dibez <pdibez@gmail.com>
2026-05-19 00:35:50 -0300

    fix: list posts page convert from dynamic to static

---
+++ src/data/blog.tsx
@@ -0,0 +1,15 @@
+export const DATA = {
+  posts: [
+    {
+      id: "hello-world",
+      data: {
+        title: "Hola Mundo!",
+        publishedAt: "2026-05-17",
+        updatedAt: "2026-05-17",
+        author: "Pablo Dibez",
+        summary: "Una introducción a mi blog",
+        image: ""
+      }
+    }    
+  ]
+} as const;
