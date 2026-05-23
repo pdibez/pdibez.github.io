e26bcd0535ec19677b2f0de59050b1552d3c5f87
Pablo Dibez <pdibez@gmail.com>
2026-05-23 11:22:54 -0300

    fix: remove days ago in post date

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -6,35 +6,15 @@ export function cn(...inputs: ClassValue[]) {
 }
 
 export function formatDate(date: string) {
-  let currentDate = new Date().getTime();
   if (!date.includes("T")) {
     date = `${date}T00:00:00`;
   }
-  let targetDate = new Date(date).getTime();
-  let timeDifference = Math.abs(currentDate - targetDate);
-  let daysAgo = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
 
   let fullDate = new Date(date).toLocaleDateString("es-ar", {
     month: "long",
     day: "numeric",
     year: "numeric",
     timeZone: "UTC",
-  });
-
-  if (daysAgo < 1) {
-    return "Hoy";
-  } else if (daysAgo = 1) {
-    return `${fullDate} (${daysAgo} dia atrás)`;
-  } else if (daysAgo < 7) {
-    return `${fullDate} (${daysAgo} dias atrás)`;
-  } else if (daysAgo < 30) {
-    const weeksAgo = Math.floor(daysAgo / 7);
-    return `${fullDate} (${weeksAgo} semanas atrás)`;
-  } else if (daysAgo < 365) {
-    const monthsAgo = Math.floor(daysAgo / 30);
-    return `${fullDate} (${monthsAgo} meses atrás)`;
-  } else {
-    const yearsAgo = Math.floor(daysAgo / 365);
-    return `${fullDate} (${yearsAgo} años atrás)`;
-  }
+  });  
+  return fullDate;
 }
